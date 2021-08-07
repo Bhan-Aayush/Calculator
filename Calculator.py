375df7370fc20788081658e54f306a48cd406a39
BhanBoy <63007327+Bhan-Aayush@users.noreply.github.com>
2021-08-07 15:41:41 -0400

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+print(num1 + num2)
+
+#use an int function(only for whole numbers!)
+result = int(num1) + int(num2)
+print(result)
+
+#For decimal numbers we only use float
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+
+result = float(num1) + float(num2)
+print(result)
