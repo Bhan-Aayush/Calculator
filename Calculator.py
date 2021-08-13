1362a3ac00690d2c9db295a9e27fe1f66f45e7a6
Plasmatic <63007327+Bhan-Aayush@users.noreply.github.com>
2021-08-12 22:44:31 -0400

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,7 +1,21 @@
-num1 = input("Enter a number: ")
-num2 = input("Enter another number: ")
-print(num1 + num2)
+num1 = float(input("Enter a number: "))
 
+op = input("Enter a number: ")
+
+num2 = float(input("Enter another number: "))
+
+if op == "+":
+  print(num1 + num2)
+
+elif op == "-":
+  print(num1 - num2)
+  
+ elif op == "/":
+  print(num1 / num2)
+
+elif op == "*":
+  print(num1 * num2)
+  
 #use an int function(only for whole numbers!)
 result = int(num1) + int(num2)
 print(result)
